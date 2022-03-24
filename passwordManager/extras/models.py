d8dd0a5029ba2a1a070026132e24e2e4834f4997
vaibhav31s <58821506+vaibhav31s@users.noreply.github.com>
2022-03-24 18:49:16 +0530

    asdfasd

--- passwordManager/extras/models.py
+++ passwordManager/extras/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Passwords(models.Model):
+    user = models.ForeignKey(User,verbose_name="",on_delete=models.CASCADE)
+    name =  models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    email  = models.CharField(max_length=100)
+    logo = models.CharField(max_length=100)
+    
+
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        ordering = ["-id"]
