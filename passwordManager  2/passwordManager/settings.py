30b1969e293636761bda9612a9ea2f1864bb7545
vaibhav31s <58821506+vaibhav31s@users.noreply.github.com>
2022-04-06 01:30:13 +0530

    trying to deploy on web

--- passwordManager  2/passwordManager/settings.py
+++ passwordManager  2/passwordManager/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i=dp)ujf*l!=3kd*!23qgw0t4_q)u0=hwkxbv2nv1vagk=z$)9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- passwordManager  2/passwordManager/urls.py
+++ passwordManager  2/passwordManager/urls.py
@@ -18,11 +18,14 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.static  import serve
+from django.conf.urls import url
 from . import urls
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("extras.urls"))
+    path('',include("extras.urls")),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]  +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
